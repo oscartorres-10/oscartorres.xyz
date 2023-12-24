60e3a0e0ac8a68819865f9682faf5f4cab5b41b2
oscartorres <38195378+oscartorres-10@users.noreply.github.com>
2023-12-24 14:23:04 -0300

    feat(project): add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://oscartorres.io"
+  site: "https://oscartorres.io",
+  integrations: [tailwind()]
 });
