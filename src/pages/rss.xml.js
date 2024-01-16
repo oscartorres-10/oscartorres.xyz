ab10369b1422d8fb8c9af357df4b14b6b33efb7e
Oscar T <38195378+oscartorres-10@users.noreply.github.com>
2024-01-16 18:28:02 -0300

    refactor(rss): update title of rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,7 +2,7 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
   return rss({
-    title: 'Oscar Torres | Blog',
+    title: 'Oscar Torres Blog',
     description: 'Oscar Torres personal website',
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
