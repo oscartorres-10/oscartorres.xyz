490ae69d8b91c5aed55898f946c884378b7aa6bd
oscartorres <38195378+oscartorres-10@users.noreply.github.com>
2024-07-23 13:19:38 -0300

    feat(rss): render and sanitize post content before passing it to rss
    so it looks properly on RSS feed readers

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,8 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(context) {
   const posts = await getCollection("posts")
@@ -12,7 +15,10 @@ export async function GET(context) {
       pubDate: post.data.pubDate,
       description: post.data.description,
       link: `/blog/${post.slug}/`,
-      content: post.body
+      // Note: this will not process components or JSX expressions in MDX files.
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+      }),
     })),
     customData: `<language>en-us</language>`,
   });
